ee22bda5f6ce04d430b59cbe74cd1f9195282422
Leandro <leandro262009@gmail.com>
2018-06-06 14:09:09 +0200

    conf.lua
    Configuration

--- conf.lua
+++ conf.lua
@@ -1,25 +1,92 @@
---Window informations
-window = {
-    mode = false,
-    width = 1280,
-    height = 720,
-    title = "Guardian of Time",
-}
-
---Background
-background = {
-    color = {
-        r = 0,
-        g = 0,
-        b = 0
-    }
-}
-
---Texts
-text = {
-    color = {
-        r = 255,
-        g = 255,
-        b = 255
-    }
+function love.conf(t) 
+    t.identity = nil                    -- The name of the save directory (string) 
+    t.version = "0.10.2"                -- The LÖVE version this game was made for (string) 
+    t.console = true                    -- Attach a console (boolean, Windows only) 
+    t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean) 
+    t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean)  
+    t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean) 
+  
+    t.window.title = "Guardian of Time" -- The window title (string) 
+    t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string) 
+    t.window.width = 1280               -- The window width (number) 
+    t.window.height = 720               -- The window height (number) 
+    t.window.borderless = false         -- Remove all border visuals from the window (boolean) 
+    t.window.resizable = false          -- Let the window be user-resizable (boolean) 
+    t.window.minwidth = 1               -- Minimum window width if the window is resizable (number) 
+    t.window.minheight = 1              -- Minimum window height if the window is resizable (number) 
+    t.window.fullscreen = false         -- Enable fullscreen (boolean) 
+    t.window.fullscreentype = "desktop" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string) 
+    t.window.vsync = false              -- Enable vertical sync (boolean) 
+    t.window.msaa = 0                   -- The number of samples to use with multi-sampled antialiasing (number) 
+    t.window.display = 1                -- Index of the monitor to show the window in (number) 
+    t.window.highdpi = false            -- Enable high-dpi mode for the window on a Retina display (boolean) 
+    t.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number) 
+    t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number) 
+  
+    t.modules.audio = true              -- Enable the audio module (boolean) 
+    t.modules.event = true              -- Enable the event module (boolean) 
+    t.modules.graphics = true           -- Enable the graphics module (boolean) 
+    t.modules.image = true              -- Enable the image module (boolean) 
+    t.modules.joystick = true           -- Enable the joystick module (boolean) 
+    t.modules.keyboard = true           -- Enable the keyboard module (boolean) 
+    t.modules.math = true               -- Enable the math module (boolean) 
+    t.modules.mouse = true              -- Enable the mouse module (boolean) 
+    t.modules.physics = true            -- Enable the physics module (boolean) 
+    t.modules.sound = true              -- Enable the sound module (boolean) 
+    t.modules.system = true             -- Enable the system module (boolean) 
+    t.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update 
+    t.modules.touch = true              -- Enable the touch module (boolean) 
+    t.modules.video = true              -- Enable the video module (boolean) 
+    t.modules.window = true             -- Enable the window module (boolean) 
+    t.modules.thread = true             -- Enable the thread module (boolean) 
+end 
+ 
+--Window informations 
+window = { 
+wdow = { 
+    mode = false, 
+    width = 1280, 
+    height = 720, 
+    wth = 1280, 
+    hgt = 720, 
+    title = "Guardian of Time", 
+} 
+ 
+--Background 
+background = { 
+    color = { 
+        r = 0, 
+        g = 0, 
+        b = 0 
+    } 
+test = "value" 
+ 
+bground = { 
+    color = {0, 0, 0} 
+} 
+ 
+--Texts 
+text = { 
+    color = { 
+        r = 255, 
+        g = 255, 
+        b = 255 
+    } 
+    color = {255, 255, 255} 
+} 
+ 
+debug = { 
+    messages = {"wdow.wth"}, 
+    color = {0, 200, 0}, 
+    draw = function() 
+        for i, v in pairs(debug.messages) do 
+            local val = 0 
+            for i = 1, #v do 
+               if string.sub(v, i, i) == "." then 
+                    val = _G[v][string.sub(v, i + 1, #v)] 
+                end 
+            end 
+            lg.print(v.." : "..val, 10, i*20) 
+        end 
+    end 
 }
